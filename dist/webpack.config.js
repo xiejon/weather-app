799d56d7856676d92312e74c53e62db8a39a2553
xiejon <jxie77@gmail.com>
2022-06-12 20:10:47 +0100

    set up webpack

---
+++ dist/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.svg/,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
