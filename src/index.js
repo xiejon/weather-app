176964e28f9d061e05c0146dcbefbcedbc0165ff
xiejon <jxie77@gmail.com>
2022-06-12 21:51:24 +0100

    cleaned code

--- src/index.js
+++ src/index.js
@@ -1,33 +1,28 @@
 const Weather = () => {
     return {
-        get(city) {
 
-            async function fetchLocation(city) {
-                const raw = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=bb9a4a8b3762bb47a3b7ff329d10b88f`, {mode: 'cors'});
-                const json = await raw.json();
-                const lat = json[0].lat;
-                const lon = json[0].lon;
-                return { lat, lon }
-            }
+        async fetchLocation(city) {
+            const raw = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=bb9a4a8b3762bb47a3b7ff329d10b88f`, {mode: 'cors'});
+            return raw.json();
+        },
 
-            async function fetchWeather(city) {
-                const loc = await fetchLocation(city);
-                const raw = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${loc.lat}&lon=${loc.lon}&appid=bb9a4a8b3762bb47a3b7ff329d10b88f`, {mode: 'cors'});
-                const json = await raw.json();
-                console.log(json);
-                return json;
-            }
+        async fetchWeather(city) {
+            const loc = await this.fetchLocation(city);
 
-            fetchWeather(city);
+            const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${loc[0].lat}&lon=${loc[0].lon}&appid=bb9a4a8b3762bb47a3b7ff329d10b88f`, {mode: 'cors'});
+            return data.json();
+        },
+
+        
 
             // Geocode 
             // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
         // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 
         //`http://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&appid=65&appid=bb9a4a8b3762bb47a3b7ff329d10b88f`
-        }
+    
     }
 }
 
 const newWidget = Weather();
-newWidget.get('downingtown, usa');
+newWidget.fetchWeather('downingtown, usa');
