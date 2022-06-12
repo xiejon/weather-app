bede819adbcf0b8ce2793e64c6e00adf450ffebf
xiejon <jxie77@gmail.com>
2022-06-12 22:09:23 +0100

    added function to convert temperature from kelvin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const Weather = () => {
+    let celcius = false;
     return {
+        celcius,
 
         async fetchLocation(city) {
             const raw = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=bb9a4a8b3762bb47a3b7ff329d10b88f`, {mode: 'cors'});
@@ -13,16 +15,18 @@ const Weather = () => {
             return data.json();
         },
 
-        
+        convertTemp(kelvin) {
+            // Kelvin -> Celcius/Fahrenheit rounded to nearest integer
+            if (celcius) {
+                return Math.round(kelvin - 273.15);
+            } else {
+                return Math.round((9/5) * (kelvin - 273.15) + 32);
+            }
+        }
 
-            // Geocode 
-            // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-        // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-
-        //`http://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&appid=65&appid=bb9a4a8b3762bb47a3b7ff329d10b88f`
-    
     }
 }
 
 const newWidget = Weather();
-newWidget.fetchWeather('downingtown, usa');
+const weather = newWidget.fetchWeather('downingtown, usa');
+weather.then(item => console.log(newWidget.convertTemp(item.main.temp)))
