1b9d78386dd21bd1f19ad5b44ea6db790f1ed9c3
xiejon <jxie77@gmail.com>
2022-06-14 20:22:21 +0100

    made loading page, added favicon, github link

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ import DrizzleSvg from "./icons/cloud-drizzle.svg";
 import RainSvg from "./icons/cloud-rain.svg";
 import LightningSvg from "./icons/cloud-lightning.svg";
 import CloudSvg from "./icons/cloud.svg";
+import RainFavicon from "./icons/raindrop.png";
 
 const WeatherObj = () => {
   let weatherData;
@@ -95,6 +96,10 @@ const UI = () => {
     nightMode,
     metric,
     showWeather(data) {
+        // Disable loading page 
+        const loadingPage = document.querySelector('.loading-page');
+        loadingPage.style.display = 'none';
+
       // Default background & icon
       this.createDefaultBackground();
       this.createDefaultSvg();
@@ -198,6 +203,7 @@ const UI = () => {
       link ? svg.setAttribute("src", link) : this.createDefaultSvg();
     },
     setImages(id) {
+        // Change background & icon according to weather ID
       if (this.nightMode) {
         if (id >= 200 && id <= 232)
           this.setBgImage(NightThunderImg), this.setIcon(LightningSvg);
@@ -223,9 +229,54 @@ const UI = () => {
         if (id === 804) this.setBgImage(OvercastImg);
       }
     },
+    setLoadingPage() {
+
+        function setFavicon() {
+            const favicon = document.createElement("link");
+            favicon.setAttribute("rel", "icon");
+            favicon.setAttribute("type", "image/x-icon");
+            favicon.setAttribute("href", RainFavicon);
+            document.head.append(favicon);
+        }
+
+        function renderGithubIcon() {
+            const a = document.createElement('a');
+            a.href = 'https://github.com/xiejon';
+
+            const footer = document.querySelector('.footer')
+            const iconSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+            const iconPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        
+            iconSvg.setAttribute('width', '24');
+            iconSvg.setAttribute('height', '24');
+            iconSvg.setAttribute('viewbox', '0 0 24 24');
+            iconPath.setAttribute('d', "M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z");
+        
+            a.append(iconSvg);
+            iconSvg.appendChild(iconPath);
+            footer.append(a)
+        }
+
+        setFavicon();
+        renderGithubIcon();
+
+        const loadingPage = document.createElement('div');
+        const content = document.createElement('p');
+
+        loadingPage.classList.add('loading-page')
+        loadingPage.style.backgroundColor = 'rgb(125, 186, 230)';
+        loadingPage.style.zIndex = '999'
+        loadingPage.style.display = 'flex';
+
+        content.textContent = 'Gathering data...'
+
+        document.body.append(loadingPage)
+        loadingPage.append(content);
+    }
   };
 };
 
+// Event listener for search button
 async function searchListener(obj, widget) {
   const search = document.querySelector(".search-div img");
   const input = document.querySelector("#search");
@@ -238,6 +289,7 @@ async function searchListener(obj, widget) {
   search.addEventListener("click", searchLoc);
 }
 
+// Event listener to toggle imperial/metric
 function metricListener(obj, widget) {
   const btn = document.querySelector(".toggle-metric");
 
@@ -261,4 +313,5 @@ async function runApp() {
   searchListener(newUI, widget);
 }
 
+UI().setLoadingPage();
 runApp();
